fa8d397fd7f53b0a77399c95371de88a564ff607
GustavoTravensoli <travensoli.lima@escola.pr.gov.br>
2026-06-25 13:25:35 -0300

    7777777

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const numeroSenha = document.querySelectorAll('.parametro-senha__texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
-numeroSenha.textContent = 5;
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-const botoes = document.querySelector('.parametro-senha__botao');
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){
+    tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha
+}
+console.log(botoes);
