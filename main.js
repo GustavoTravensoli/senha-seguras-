3df84c6506455a6a1db735e473cff8f1d42f822b
GustavoTravensoli <travensoli.lima@escola.pr.gov.br>
2026-06-25 13:04:43 -0300

    ggggggggggggggg

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-document.querySelectorAll('.parametro-senha__texto')
+const numeroSenha = document.querySelectorAll('.parametro-senha__texto');
+
+numeroSenha.textContent = 5;
+
+const botoes = document.querySelector('.parametro-senha__botao');
